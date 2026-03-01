aada8ac188690f4c36286c29a214f68bbcd5e112
ThomasRicart <ricart@laposte.net>
2026-03-01 22:49:36 +0100

    c

--- deploy_push_clean.bat
+++ deploy_push_clean.bat
@@ -1,4 +1,17 @@
 @echo off
+del /S /Q *.aux *.pytxcode *.thm *.log *.out *.toc *.lof *.lot *.nav *.snm *.synctex.gz *.bbl *.blg *.fls *.aux *.xsim *.fdb_latexmk
+
+@echo off
+setlocal enabledelayedexpansion
+
+:: Recherche récursive des dossiers "pythontex-files*"
+for /d /r %%D in (pythontex-files*) do (
+    echo Suppression du dossier : "%%D"
+    rd /s /q "%%D"
+)
+
+echo Suppression terminée.
+
 set /p message="Entrez votre message de commit (ex: Mise a jour doc) : "
 
 echo --- Debut du processus complet ---
